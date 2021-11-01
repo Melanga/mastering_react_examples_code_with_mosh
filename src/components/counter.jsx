914117c8f4d2c30ebfa551e6080752803e2e31ff
melanga <dissanayakedmmb@gmail.com>
2021-11-01 17:58:04 +0530

    added increment handlers and delete handlers. But Reset not working becasue local state in counter.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,87 +1,44 @@
 import React, { Component } from "react";
-import { getMovies } from "../services/fakeMovieService";
 
 class Counter extends Component {
   state = {
-    imageUrl: "https://picsum.photos/200",
-    movies: getMovies(),
-    count: getMovies().length,
+    value: this.props.counter.value,
   };
 
-  handleDelete = (movie) => {
-    const movies = this.state.movies.filter((m) => m._id !== movie._id);
-    this.setState({ movies, count: movies.length });
+  // arrow functions automatically refer to the current this without referring to window
+  handleIncrement = () => {
+    // console.log("clicked", this);
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
-    const { length: count } = this.state.movies;
-    if (count === 0)
-      return (
-        <React.Fragment>
-          <span className={this.getBadgeStyleClass()}>
-            {this.formatCount()}
-          </span>
-        </React.Fragment>
-      );
     return (
-      <React.Fragment>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
+      <div>
         <span className={this.getBadgeStyleClass()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
-          increment
+          Increment
         </button>
-        <div>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </React.Fragment>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
-  // arrow functions automatically refer to the current this without referring to window
-  handleIncrement = () => {
-    // console.log("clicked", this);
-    this.setState({ count: this.state.count + 1 });
-  };
-
   getBadgeStyleClass() {
     let badgeStyle = "badge m-2 bg-";
-    badgeStyle += this.state.count === 0 ? "warning" : "primary";
+    badgeStyle += this.state.value === 0 ? "warning" : "primary";
     return badgeStyle;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/counter'
+import Counters from './components/counters'
 import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>,
   document.getElementById('root')
 );
